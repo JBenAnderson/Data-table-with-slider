0c0c5a23bd965938eed6a5dce6bfa401bf09008a
J Ben Anderson <revovisions@gmail.com>
2022-06-06 14:07:39 -0400

    added chakra slider imports and components to app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,19 @@ import {
   Code,
   Grid,
   theme,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+  RangeSlider,
+  RangeSliderTrack,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  Tooltip,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
@@ -18,21 +31,23 @@ function App() {
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
+          <VStack spacing={8}></VStack>
+          <RangeSlider
+            defaultValue={[0, 30]}
+            colorScheme="blue"
+            size="lg"
+            onChange="{}"
+            onChangeEnd="{}"
+            min="{}"
+            max="{}"
+            step={1}
+            width="600px"
+            height="50px"
+          >
+            <RangeSliderTrack index={0}>
+              <RangeSliderFilledTrack />
+            </RangeSliderTrack>
+          </RangeSlider>
         </Grid>
       </Box>
     </ChakraProvider>
